f8b3f4de5e43d8cbd3c41627db95301ce1a173e2
DavynPascual <Davynp1@gmail.com>
2023-10-17 10:25:18 -0600

    updated page

--- app/page.js
+++ app/page.js
@@ -6,7 +6,19 @@ export default function Home() {
         <main>
             <h1>CPRG 306: Web Development 2 - Assignments</h1>
             <Myinfo />
-            <Link href="http://localhost:3001/week2">Week 2</Link>
+            
+           <div> 
+           <Link href="/week2">Week 2</Link>
+           </div>
+           <div>
+            <Link href="/Week3">Week 3</Link>
+            </div>
+            <div>
+            <Link href="/week4">Week 4</Link>
+            </div>
+
+
+
         </main>
     )
 }
---
+++ app/week4/page.js
@@ -0,0 +1,9 @@
+import NewItem from "./new-item";
+
+export default function page() {
+    return (
+        <main className="flex justify-center w-full ">
+            <NewItem /> 
+        </main>
+    )
+}
